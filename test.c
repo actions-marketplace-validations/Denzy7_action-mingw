9944676a6186d55d10031903b84d4a5a8474389e
Denzy7 <dennismyner7@gmail.com>
2024-02-12 15:08:56 +0300

    add test

---
+++ test.c
@@ -0,0 +1,6 @@
+#include <stdio.h>
+int main(int argc, char *argv[])
+{
+    printf("test_c\n");
+    return 0;
+}
---
+++ test.cpp
@@ -0,0 +1,6 @@
+#include <iostream>
+int main(int argc, char *argv[])
+{
+    std::cout << "test_cxx" << std::endl;
+    return 0;
+}
